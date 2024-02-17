38e3875503be963b337f2e8aef343ae388ff6de1
AdminLarkos <michigoebel94@googlemail.com>
2024-02-17 21:00:33 +0100

    feat: added first connection attempt

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BurningStarDiscordBot
+
+
+https://realpython.com/how-to-make-a-discord-bot-python/#how-to-make-a-discord-bot-in-python
--- src/startup.py
+++ src/startup.py
@@ -1,1 +1,19 @@
-print('helloWorld')
+import os
+
+import discord
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_message(message):
+    if 'happy birthday' in message.content.lower():
+        await message.channel.send('Happy Birthday! 🎈🎉')
+
+client.run(TOKEN)
+
